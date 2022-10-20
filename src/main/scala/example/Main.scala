b4f08e67428acba3dbe44cb4361532e10d2f4490
amilne-mdsol <amilne@mdsol.com>
2022-10-20 11:04:45 +0100

    Challenge 1

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -5,6 +5,22 @@ object Main {
   def main(args: Array[String]): Unit =
     println("Hello com.example.Empty Project!")
 
+  /*
+  SCALA GOLF CHALLENGE #1
+  Write a function that when passed a string will return a true only if all the first instances of a double letter combination are in reverse alphabetical order
+
+  e.g.
+  input - azzbmmzdd -> true as the double letters zz, mm and dd are in z->a order.
+  input - azzbmmzziee -> true as the second zz has already occurred, so can be ignored
+  input - aabbrr -> false as the double letters are not in z->a order
+
+  There are a series of unit tests in this project that need to all pass for your submission to be considered.
+
+  You will be scored for the least number of characters you use in your function(s). White space, carriage returns, etc, all count.
+
+  Please use the function challengeFunction below.
+   */
+
   def challengeFunction(inputString: String): Boolean = {
     true
   }
--- src/test/scala/example/ExampleSpec.scala
+++ src/test/scala/example/ExampleSpec.scala
@@ -7,6 +7,38 @@ class ExampleSpec extends FunSuite {
   val mainFunction: Main.type = Main
 
   test("challenge test 1") {
+    assert(mainFunction.challengeFunction("azzbmmzdd"))
+  }
+
+  test("challenge test 2") {
+    assert(mainFunction.challengeFunction("azzbmmzziee"))
+  }
+
+  test("challenge test 3") {
+    assert(!mainFunction.challengeFunction("aabbrr"))
+  }
+
+  test("challenge test 4") {
     assert(mainFunction.challengeFunction(""))
+  }
+
+  test("challenge test 5") {
+    assert(mainFunction.challengeFunction("aaaaaaaaaaaaaaaaa111aaaaaaaa985428745aaaa"))
+  }
+
+  test("challenge test 6") {
+    assert(!mainFunction.challengeFunction("fffffffffffffzzzyybbbbbbbbaaabbbbbaa111aaaaaaaa985428745aaaa"))
+  }
+
+  test("challenge test 7") {
+    assert(mainFunction.challengeFunction("IlIlIlIlIl1iIlI1Il!l!liI1IL"))
+  }
+
+  test("challenge test 8") {
+    assert(mainFunction.challengeFunction("zzzyyxxccbbaabbccxxyyzzzzzzz"))
+  }
+
+  test("challenge test 9") {
+    assert(!mainFunction.challengeFunction("0OO00OO00OO00QQ@@OO00"))
   }
 }
