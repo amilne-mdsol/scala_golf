644dd6f133d9bae88ad98be7d677edb5560e562f
amilne-mdsol <amilne@mdsol.com>
2022-11-01 18:54:29 +0000

    submissions

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -21,11 +21,33 @@ object Main {
   Please use the function challengeFunction below.
    */
 
-  def challengeFunction(inputString: String): Boolean = {
+  def challengeFunctionTom(a: String) = {
+    var l = -1;
+    for {
+      p <- Range(122, 97, -1)
+      i = a.toLowerCase.indexOf(p.toChar.toString * 2)
+    } yield if (i >= 0) if (i > l) l = i else return false;
+    true
+  }
+  //186 characters
+
+  def challengeFunctionLuke(s: String) = {
+    val f = s.zip(s.tail).filter(x => x._1 == x._2).distinct; f == f.sortBy(-_._1)
+  }
+  //103 characters
+
+  def challengeFunctionAmir(inputString: String): Boolean = {
+    if (inputString.isEmpty) true
+    else if (".*(.)\\1.*".r.matches(inputString)) {
+      val s1 = inputString.toList.zip(inputString.drop(1)).filter(c => c._1 == c._2).distinct.map(_._1)
+      s1.reverse == s1.reverse.sorted
+    } else true
+  }
+  //272 characters
+
+  def challengeFunctionAlec(inputString: String): Boolean = {
     val brokenDownInputStringWithIndex: Seq[(Char, Int)] = inputString.toCharArray.toSeq.zipWithIndex
-
     val brokenDownInputStringWithIndexWithoutLast = brokenDownInputStringWithIndex.dropRight(1)
-
     val onlyDuplicatedCharactersInBrokenDownInputStringWithIndexSeqSeq: Seq[Seq[Char]] =
       brokenDownInputStringWithIndexWithoutLast.map {
         case (thisValue, index) =>
@@ -36,10 +58,8 @@ object Main {
             Seq.empty
           }
       }
-
     val onlyDuplicatedCharactersInBrokenDownInputStringWithIndex: Seq[(Char, Int)] =
       onlyDuplicatedCharactersInBrokenDownInputStringWithIndexSeqSeq.flatten.zipWithIndex
-
     val inputStringWithoutDuplicatesReduceDoublesToSinglesSeqSeq: Seq[Seq[Char]] =
       onlyDuplicatedCharactersInBrokenDownInputStringWithIndex.map {
         case (thisValue, index) =>
@@ -51,13 +71,38 @@ object Main {
             Seq(thisValue)
           }
       }
-
     val inputStringWithoutDuplicatesReduceDoublesToSingles =
       inputStringWithoutDuplicatesReduceDoublesToSinglesSeqSeq.flatten
-
     val inputStringWithoutDuplicatesReduceDoublesToSinglesSortedReverse =
       inputStringWithoutDuplicatesReduceDoublesToSingles.sorted.reverse
-
     inputStringWithoutDuplicatesReduceDoublesToSingles == inputStringWithoutDuplicatesReduceDoublesToSinglesSortedReverse
   }
+  //1472 characters
+
+  def challengeFunction(a: String) = {
+    val b = a.toSeq.zipWithIndex
+    val c =
+      b.dropRight(1)
+        .flatMap {
+          case (v, i) =>
+            if (v == b.apply(i + 1)._1) {
+              Seq(v)
+            } else {
+              Seq.empty
+            }
+        }
+        .zipWithIndex
+    val d =
+      c.flatMap {
+        case (v, i) =>
+          if (c.take(i).map(_._1).contains(v)) {
+            Seq.empty
+          } else {
+            Seq(v)
+          }
+      }
+    d == d.sorted.reverse
+  }
+  //276 characters
+
 }
