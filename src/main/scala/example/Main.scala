3da44054eb2936d19a69a3757be53e7597f1a63f
amilne-mdsol <amilne@mdsol.com>
2022-11-01 19:30:21 +0000

    Challenge 2 setup

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -5,7 +5,19 @@ object Main {
   def main(args: Array[String]): Unit =
     println("Hello com.example.Empty Project!")
 
-  def challengeFunction(inputString: String): Boolean = {
-    true
+  /*
+  CHALLENGE 2
+  The input string will be a positive integer then a comma then another positive integer
+
+  What do I do with these two positive integers?
+
+  Convert the first number to binary, with the length of the output string dictated by the second integer
+
+  "10,5" => "01010"
+  "7,10" => "0000000111"
+   */
+
+  def challengeFunction(inputString: String): String = {
+    ""
   }
 }
--- src/test/scala/example/ExampleSpec.scala
+++ src/test/scala/example/ExampleSpec.scala
@@ -7,6 +7,22 @@ class ExampleSpec extends FunSuite {
   val mainFunction: Main.type = Main
 
   test("challenge test 1") {
-    assert(mainFunction.challengeFunction(""))
+    assert(mainFunction.challengeFunction("10,5") == "01010")
+  }
+
+  test("challenge test 2") {
+    assert(mainFunction.challengeFunction("7,10") == "0000000111")
+  }
+
+  test("challenge test 3") {
+    assert(mainFunction.challengeFunction("128,5") == "00000")
+  }
+
+  test("challenge test 4") {
+    assert(mainFunction.challengeFunction("127,7") == "1111111")
+  }
+
+  test("challenge test 5") {
+    assert(mainFunction.challengeFunction("0,7") == "0000000")
   }
 }
