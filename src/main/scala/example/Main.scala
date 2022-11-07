d3a0f98bd4fe04946a568e119e0e4136fad893d0
amilne-mdsol <amilne@mdsol.com>
2022-11-07 08:22:00 +0000

    Setup for Challenge 3

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -5,7 +5,23 @@ object Main {
   def main(args: Array[String]): Unit =
     println("Hello com.example.Empty Project!")
 
-  def challengeFunction(inputString: String): Boolean = {
-    true
+  /*
+  Challenge 3: Find the mountains
+  The function will be given a Seq of Integers
+  You need to output a Seq of Truples of all the "peaks" in the data
+
+  e.g.
+  Input: 1,5,1,6,7,8,7,6,8
+  Output: (1,5,1),(7,8,7)
+
+  Input: 1,2,3,4,5,6,7,8,9,1
+  Output: (8,9,1)
+
+  Input: 0,0,0,0,5,0,0,0,0
+  Output: (0,5,0)
+   */
+
+  def challengeFunction(inputSeq: Seq[Int]): Seq[(Int, Int, Int)] = {
+    Seq((0, 0, 0))
   }
 }
--- src/test/scala/example/ExampleSpec.scala
+++ src/test/scala/example/ExampleSpec.scala
@@ -7,6 +7,14 @@ class ExampleSpec extends FunSuite {
   val mainFunction: Main.type = Main
 
   test("challenge test 1") {
-    assert(mainFunction.challengeFunction(""))
+    assert(mainFunction.challengeFunction(Seq(1, 5, 1, 6, 7, 8, 7, 6, 8)) == Seq((1, 5, 1), (7, 8, 7)))
+  }
+
+  test("challenge test 2") {
+    assert(mainFunction.challengeFunction(Seq(1, 2, 3, 4, 5, 6, 7, 8, 9, 1)) == Seq((8, 9, 1)))
+  }
+
+  test("challenge test 3") {
+    assert(mainFunction.challengeFunction(Seq(0, 0, 0, 0, 5, 0, 0, 0, 0)) == Seq((0, 5, 0)))
   }
 }
