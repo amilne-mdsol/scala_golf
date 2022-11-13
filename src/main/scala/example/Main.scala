066077df0890ccac32a9fa42fdd35909026d6a67
amilne-mdsol <amilne@mdsol.com>
2022-11-13 22:13:29 +0000

    Quality of life improvements

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -1,7 +1,76 @@
 package example
 
 object Main {
-  def challengeFunction(i: String): Boolean = {
-    true
+
+  /*
+    Challenge 8
+    Keyboard distance
+
+    Given a string representing array of characters, give the minimum overall distance a finger would have to travel to type in the given word.
+
+    Note: There can be duplicate letters in the keyboard layout.
+    You will need to find the shortest total distance taking these multiple options into consideration.
+
+    This challenge has two ways to win
+
+    1 - Shortest code solution. Use the fewest non-space characters possible to fulfill all the test scenarios.
+    2 - Fastest code solution. Write the most clock-cycle efficient method for solving this problem.
+
+    Notes:
+    The function declaration is not to be altered. These characters will not be added to the character count.
+    Round the result to a 1 decimal place Double
+    Keys are 1 unit square and there is no gap between keys.
+    If the input word uses a letter that isn't in the keyboard then return -1.0 for the result
+    No Variables, this is Scala, we use Values.
+
+    Example and explanation:
+
+    challenge test 1
+    assertEquals(mainFunction.challengeFunction("""qwertyuiop
+      |asdfghjkl
+      |zxcvbnm""".stripMargin, "inputword"), 28.7)
+
+    This means that the keyboard would be like this:
+    qwertyuiop
+    asdfghjkl-
+    zxcvbnm---
+
+    Which is just a slightly shifted version of the regular English language keyboard
+
+    Imagine you are typing with just one finger going from letter to letter in a perfectly straight line
+
+    If the word was "it"
+    You would start with your finger above the "i"
+    Move to the left 3 buttons to the "t" letter
+    This is a distance of 3, so the output from this keyboard for the word "it" would be 3.0
+
+    Typing the word "inputword" is a bit longer, but just the same calculation for each letter.
+
+    Moving from letter to letter in a big zigzag pattern for the whole word results in a distance of 28.7 (rounded to 1dp)
+
+    Here is the input keyboard with all the unused letters removed for display purposes.
+    -w-rt-uiop
+    --d-------
+    -----n----
+
+    i -> n = down 2 left 2 (sqrt(2^2 + 2^2)) approx 2.8
+    n -> p = up 2 right 4 approx 4.5
+    p -> u = left 3
+    u -> t = left 2
+    t -> w = left 3
+    w -> o = right 7
+    o -> r = left 5
+    r -> d = down 1 left 1 approx 1.4
+
+    Adding all these together equal 28.7
+
+    In the second test there is another "w" letter that results in a shorter distance overall,
+    so the second test has a very similar keyboard design (with the extra w at the end) but a shorter distance of 24.6
+
+   */
+
+  def challengeFunction(k: String, w: String): Double = {
+    0.0d
   }
+
 }
--- src/test/scala/example/ExampleSpec.scala
+++ src/test/scala/example/ExampleSpec.scala
@@ -6,7 +6,128 @@ class ExampleSpec extends FunSuite {
 
   val mainFunction: Main.type = Main
 
+  test("challenge test easy 1") {
+    assertEquals(mainFunction.challengeFunction(
+      """qwertyuiop
+        |asdfghjkl
+        |zxcvbnm""".stripMargin, "i"), 0.0)
+  }
+
+  test("challenge test easy 2") {
+    assertEquals(mainFunction.challengeFunction(
+      """qwertyuiop
+        |asdfghjkl
+        |zxcvbnm""".stripMargin, "qp"), 9.0)
+  }
+
+  test("challenge test easy 3") {
+    assertEquals(mainFunction.challengeFunction(
+      """qwertyuiop
+        |asdfghjkl
+        |zxcvbnm""".stripMargin, "in"), 2.8)
+  }
+
+  test("challenge test easy 4") {
+    assertEquals(mainFunction.challengeFunction(
+      """qwertyuiop
+        |asdfghjkl
+        |zxcvbnm""".stripMargin, "IN"), 2.8)
+  }
+
+  test("challenge test easy 5") {
+    assertEquals(mainFunction.challengeFunction(
+      """qwertyuiop
+        |asdfghjkl
+        |zxcvbnm""".stripMargin, "ini"), 5.7)
+  }
+
+  test("challenge test easy 6") {
+    assertEquals(mainFunction.challengeFunction(
+      """qwertyuiop
+        |asdfghjkl!
+        |zxcvbnm""".stripMargin, "ini!"), 7.9)
+  }
+
   test("challenge test 1") {
-    assertEquals(mainFunction.challengeFunction(""), true)
+    assertEquals(mainFunction.challengeFunction("""qwertyuiop
+        |asdfghjkl
+        |zxcvbnm""".stripMargin, "inputword"), 28.7)
+  }
+
+  test("challenge test 2") {
+    assertEquals(mainFunction.challengeFunction("""qwertyuiop
+        |asdfghjkl
+        |zxcvbnmw""".stripMargin, "inputword"), 24.6)
+  }
+
+  test("challenge test 3") {
+    assertEquals(mainFunction.challengeFunction("""qwertyuop
+        |asdfghjkl
+        |zxcvbnmw""".stripMargin, "InputWord"), -1.0)
+  }
+
+  test("challenge test 4") {
+    assertEquals(mainFunction.challengeFunction(
+      """qwertyuiop
+        |asdfghjkl
+        |
+        |zxcvbnmw""".stripMargin, "inputword"), 27.4)
+  }
+
+  test("challenge test 5") {
+    assertEquals(mainFunction.challengeFunction(
+      """a
+        |b
+        |bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbz""".stripMargin, "az"), 32.1)
+  }
+
+  test("challenge test 6") {
+    assertEquals(mainFunction.challengeFunction(
+      """a
+        |b
+        |bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbz""".stripMargin, "abz"), 32.1)
+  }
+
+  test("challenge test 7") {
+    assertEquals(mainFunction.challengeFunction(
+      """a
+        |b
+        |bbbbbbbbbbz""".stripMargin, "baaabbzzzbbb"), 12.2)
+  }
+
+  test("challenge test 8") {
+    assertEquals(
+      mainFunction.challengeFunction(
+        """zazzzzzzzzzzzzzzzzzya
+          |bzzzzzzzzzzzzzzzbzzzz
+          |zazzzzzzzzzzzzzzzzzxa""".stripMargin,
+        "yabax",
+      ),
+      10.2,
+    )
+  }
+
+  test("challenge test 9") {
+    assertEquals(
+      mainFunction.challengeFunction(
+        """a                            a
+          |b                         b
+          |cd                           cd               z""".stripMargin,
+        "abcdabcdz",
+      ),
+      32.9,
+    )
+  }
+
+  test("challenge test 10") {
+    assertEquals(
+      mainFunction.challengeFunction(
+        """abbbbbbbbbbbbbbbbbz
+          |b
+          |abbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbz""".stripMargin,
+        "baaabbzzzbbbaaabbzzzbbaaabbzzzbbaaabbzzzbbaaabbzzzbbaaabbzzzbb",
+      ),
+      200.0,
+    )
   }
 }
