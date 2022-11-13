a90c620ceef7b4667d1312dda93eb54ab2c1a30a
amilne-mdsol <amilne@mdsol.com>
2022-11-13 22:13:29 +0000

    Quality of life improvements

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -1,11 +1,7 @@
 package example
 
 object Main {
-
-  def main(args: Array[String]): Unit =
-    println("Hello com.example.Empty Project!")
-
-  def challengeFunction(inputString: String): Boolean = {
+  def challengeFunction(i: String): Boolean = {
     true
   }
 }
--- src/test/scala/example/ExampleSpec.scala
+++ src/test/scala/example/ExampleSpec.scala
@@ -7,6 +7,6 @@ class ExampleSpec extends FunSuite {
   val mainFunction: Main.type = Main
 
   test("challenge test 1") {
-    assert(mainFunction.challengeFunction(""))
+    assertEquals(mainFunction.challengeFunction(""), true)
   }
 }
