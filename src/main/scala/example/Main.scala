bb142e3b0c31e20cd3ec464b826b12db154ca2ca
amilne-mdsol <amilne@mdsol.com>
2022-11-14 11:58:06 +0000

    Challenge 4 setup - number to number

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -1,7 +1,24 @@
 package example
 
 object Main {
-  def challengeFunction(i: String): Boolean = {
-    true
+
+  /*
+  Challenge 4:
+
+  Convert the integer input to a string output.
+
+  Sounds easy?
+
+  WRONG
+
+  You have to convert the integer into a written version of the number
+
+  1 => One
+  2 => Two
+  95716 => Ninety five thousand, seven hundred and sixteen
+   */
+
+  def challengeFunction(i: Int): String = {
+    ""
   }
 }
