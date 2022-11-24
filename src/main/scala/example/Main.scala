02af4437560d62f6cb0ac1a9b2f4ed651197278e
amilne-mdsol <amilne@mdsol.com>
2022-11-23 21:18:15 -0600

    Challenge 5 setup, leeroy jenkins

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -1,7 +1,31 @@
 package example
 
 object Main {
-  def challengeFunction(i: String): Boolean = {
-    true
+
+  /*
+  Challenge 5
+  Leeeeeeeeeroooooooooy Jeeeeeeeeenkiiiiiiiiins!
+
+  Challenge this time is to make dynamically recreate a Leeroy Jenkins type output.
+
+  Inputs:
+  Integer and a String
+
+  Expand or contract the instances of vowels in the String according to the integer values.
+
+  NO VARIABLES OR MUTABLE TYPES
+
+  E.g.
+  92
+  abracadabra
+
+  aaaaaaaaabraacaaaaaaaaadaabraaaaaaaaa
+
+  Explanation:
+  Alternate expanding the vowels by 9 then 2 then 9 then 2 then 9 then 2 etc.
+   */
+
+  def challengeFunction(i: Int, s: String): String = {
+    s
   }
 }
--- src/test/scala/example/ExampleSpec.scala
+++ src/test/scala/example/ExampleSpec.scala
@@ -7,6 +7,44 @@ class ExampleSpec extends FunSuite {
   val mainFunction: Main.type = Main
 
   test("challenge test 1") {
-    assertEquals(mainFunction.challengeFunction(""), true)
+    assertEquals(mainFunction.challengeFunction(1, "abracadabra"), "abracadabra")
+  }
+
+  test("challenge test 2") {
+    assertEquals(mainFunction.challengeFunction(3, "abracadabra"), "aaabraaacaaadaaabraaa")
+  }
+
+  test("challenge test 3") {
+    assertEquals(mainFunction.challengeFunction(13, "abracadabra"), "abraaacadaaabra")
+  }
+
+  test("challenge test 4") {
+    assertEquals(mainFunction.challengeFunction(13, "aaaaaabraaaaaacaaaaaadaaaaaabraaaaaa"), "abraaacadaaabra")
+  }
+
+  test("challenge test 5") {
+    assertEquals(mainFunction.challengeFunction(3912511, "abracadabra"), "aaabraaaaaaaaacadaabraaaaa")
+  }
+
+  test("challenge test 6") {
+    assertEquals(mainFunction.challengeFunction(9, "Leeroy Jenkins"), "Leeeeeeeeeroooooooooy Jeeeeeeeeenkiiiiiiiiins")
+  }
+
+  test("challenge test 7") {
+    assertEquals(mainFunction.challengeFunction(1, "whooooooooooooooooop"), "whop")
+  }
+
+  test("challenge test 8") {
+    assertEquals(
+      mainFunction.challengeFunction(2, "aeiouaeiouaeiouaeiouaeiouaeiouaeiou"),
+      "aaeeiioouuaaeeiioouuaaeeiioouuaaeeiioouuaaeeiioouuaaeeiioouuaaeeiioouu",
+    )
+  }
+
+  test("challenge test 9") {
+    assertEquals(
+      mainFunction.challengeFunction(33, "aeiouaeiouaeiouaeiouaeiouaeiouaeiou"),
+      "aaaeeeiiiooouuuaaaeeeiiiooouuuaaaeeeiiiooouuuaaaeeeiiiooouuuaaaeeeiiiooouuuaaaeeeiiiooouuuaaaeeeiiiooouuu",
+    )
   }
 }
