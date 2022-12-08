0b2c22eeffb187023e979edff96dbbb79003574a
amilne-mdsol <amilne@mdsol.com>
2022-12-08 16:08:51 +0000

    Challenge 6 - boxes setup

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -1,7 +1,37 @@
 package example
 
 object Main {
-  def challengeFunction(i: String): Boolean = {
-    true
+
+  /*
+  Challenge 6 - boxes
+
+  Given two integers in, create an output that draws an array of boxes in a string for the output.
+
+  Size of the boxes are (including edge characters) 6 wide and 3 tall. All required ASCII characters have been supplied outside of your required solution.
+
+  e.g.
+  input 2,2
+  output
+  ┌────┬────┐
+  │    │    │
+  ├────┼────┤
+  │    │    │
+  └────┴────┘
+
+  input 2,3
+  output
+  ┌────┬────┐
+  │    │    │
+  ├────┼────┤
+  │    │    │
+  ├────┼────┤
+  │    │    │
+  └────┴────┘
+   */
+
+  private val c = "─│┌┬┐├┼┤└┴┘"
+
+  def challengeFunction(x: Int, y: Int): String = {
+    ""
   }
 }
--- src/test/scala/example/ExampleSpec.scala
+++ src/test/scala/example/ExampleSpec.scala
@@ -7,6 +7,157 @@ class ExampleSpec extends FunSuite {
   val mainFunction: Main.type = Main
 
   test("challenge test 1") {
-    assertEquals(mainFunction.challengeFunction(""), true)
+    assertEquals(
+      mainFunction.challengeFunction(2, 2),
+      "┌────┬────┐\n" +
+        "│    │    │\n" +
+        "├────┼────┤\n" +
+        "│    │    │\n" +
+        "└────┴────┘",
+    )
+  }
+
+  test("challenge test 2") {
+    assertEquals(
+      mainFunction.challengeFunction(2, 3),
+      "┌────┬────┐\n" +
+        "│    │    │\n" +
+        "├────┼────┤\n" +
+        "│    │    │\n" +
+        "├────┼────┤\n" +
+        "│    │    │\n" +
+        "└────┴────┘",
+    )
+  }
+
+  test("challenge test 3") {
+    assertEquals(
+      mainFunction.challengeFunction(1, 1),
+      "┌────┐\n" +
+        "│    │\n" +
+        "└────┘",
+    )
+  }
+
+  test("challenge test 4") {
+    assertEquals(
+      mainFunction.challengeFunction(1, 5),
+      "┌────┐\n" +
+        "│    │\n" +
+        "├────┤\n" +
+        "│    │\n" +
+        "├────┤\n" +
+        "│    │\n" +
+        "├────┤\n" +
+        "│    │\n" +
+        "├────┤\n" +
+        "│    │\n" +
+        "└────┘",
+    )
+  }
+
+  test("challenge test 5") {
+    assertEquals(
+      mainFunction.challengeFunction(5, 1),
+      "┌────┬────┬────┬────┬────┐\n" +
+        "│    │    │    │    │    │\n" +
+        "└────┴────┴────┴────┴────┘",
+    )
+  }
+
+  test("challenge test 6") {
+    assertEquals(
+      mainFunction.challengeFunction(5, 5),
+      "┌────┬────┬────┬────┬────┐\n" +
+        "│    │    │    │    │    │\n" +
+        "├────┼────┼────┼────┼────┤\n" +
+        "│    │    │    │    │    │\n" +
+        "├────┼────┼────┼────┼────┤\n" +
+        "│    │    │    │    │    │\n" +
+        "├────┼────┼────┼────┼────┤\n" +
+        "│    │    │    │    │    │\n" +
+        "├────┼────┼────┼────┼────┤\n" +
+        "│    │    │    │    │    │\n" +
+        "└────┴────┴────┴────┴────┘",
+    )
+  }
+
+  test("challenge test 7") {
+    assertEquals(
+      mainFunction.challengeFunction(2, 1),
+      "┌────┬────┐\n" +
+        "│    │    │\n" +
+        "└────┴────┘",
+    )
+  }
+
+  test("challenge test 8") {
+    assertEquals(
+      mainFunction.challengeFunction(1, 2),
+      "┌────┐\n" +
+        "│    │\n" +
+        "├────┤\n" +
+        "│    │\n" +
+        "└────┘",
+    )
+  }
+
+  test("challenge test 9") {
+    assertEquals(
+      mainFunction.challengeFunction(0, 0),
+      "",
+    )
+  }
+
+  test("challenge test 10") {
+    assertEquals(
+      mainFunction.challengeFunction(0, 9),
+      "",
+    )
+  }
+
+  test("challenge test 11") {
+    assertEquals(
+      mainFunction.challengeFunction(9, 0),
+      "",
+    )
+  }
+
+  test("challenge test 12") {
+    assertEquals(
+      mainFunction.challengeFunction(1, 11),
+      "┌────┐\n" +
+        "│    │\n" +
+        "├────┤\n" +
+        "│    │\n" +
+        "├────┤\n" +
+        "│    │\n" +
+        "├────┤\n" +
+        "│    │\n" +
+        "├────┤\n" +
+        "│    │\n" +
+        "├────┤\n" +
+        "│    │\n" +
+        "├────┤\n" +
+        "│    │\n" +
+        "├────┤\n" +
+        "│    │\n" +
+        "├────┤\n" +
+        "│    │\n" +
+        "├────┤\n" +
+        "│    │\n" +
+        "├────┤\n" +
+        "│    │\n" +
+        "└────┘",
+    )
+  }
+
+  test("challenge test 13") {
+    assertEquals(
+      mainFunction.challengeFunction(11, 1),
+      "┌────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┐\n" +
+        "│    │    │    │    │    │    │    │    │    │    │    │\n" +
+        "└────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┘",
+    )
   }
 }
