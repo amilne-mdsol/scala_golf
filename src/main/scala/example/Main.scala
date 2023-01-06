2d34bcf9e852023b522637114e8d24a3369e521e
amilne-mdsol <amilne@mdsol.com>
2023-01-06 13:48:52 +0000

    challenge 6 submissions

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -31,7 +31,44 @@ object Main {
 
   private val c = "─│┌┬┐├┼┤└┴┘"
 
-  def challengeFunction(x: Int, y: Int): String = {
-    ""
-  }
+  def challengeFunctionAlec(x: Int, y: Int): String = {
+    def Z(q: Int) = c(q * 3 + 2) + "─" * 4 + (c(q * 3 + 3) + "─" * 4) * (x - 1) + c(q * 3 + 4)
+    val t = "\n" + "│    " * x + "│\n"
+
+    if (x > 0 & y > 0) Z(0) + t + (Z(1) + t) * (y - 1) + Z(2) else ""
+  } //122
+
+  def challengeFunctionLuke(x: Int, y: Int): String = {
+    def f = if (_) 1 else 0
+
+    0 to y collect { case i if x * y > 0 =>
+      0 to x map { j =>
+        c(10 - (f(i < 1) + f(i < y)) * 3 - f(j < 1) - f(j < x))
+      } mkString "─" * 4
+    } mkString s"\n│${"    │" * x}\n"
+  } //129
+
+  def challengeFunctionAamir(x: Int, y: Int): String = {
+    type S = String
+
+    def l(t: S, m: S, n: S, e: S) = {
+      var s = t
+      for (i <- 1 until (x * 6) - x) {
+        if (i % 5 == 0) s = s + m
+        else s = s + n
+      }
+      s + e + "\n"
+    }
+
+    if (x == 0 || y == 0) ""
+    else {
+      var s = l("┌", "┬", "─", "┐")
+      for (i <- 1 until 2 * y) {
+        if (i % 2 != 0) s = s + l("│", "│", " ", "│")
+        else s = s + l("├", "┼", "─", "┤")
+      }
+      s = s + l("└", "┴", "─", "┘")
+      s.init
+    }
+  } //246
 }
