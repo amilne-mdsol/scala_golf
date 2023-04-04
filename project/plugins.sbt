ca2900a97e97ea7f831782f15f2e525849c81743
amilne-mdsol <amilne@mdsol.com>
2023-04-04 20:02:08 +0100

    Adding benchmarking with JMH

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,2 +1,3 @@
 addSbtPlugin("io.github.davidgregory084" % "sbt-tpolecat" % "0.3.3")
 addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.4.6")
+addSbtPlugin("pl.project13.scala" % "sbt-jmh" % "0.4.3")
--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -1,6 +1,20 @@
 package example
 
+import org.openjdk.jmh.annotations.{Benchmark, BenchmarkMode, Mode, OutputTimeUnit}
+import org.openjdk.jmh.infra.Blackhole
+
+import java.util.concurrent.TimeUnit
+
+@BenchmarkMode(Array(Mode.AverageTime))
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
 object Main {
+
+  @Benchmark
+  def testBenchmarkFunction(bh: Blackhole): Unit = {
+    val w = ""
+    bh.consume(challengeFunction(w))
+  }
+
   def challengeFunction(i: String): Boolean = {
     true
   }
