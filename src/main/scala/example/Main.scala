77bd6ddd872d56e31cb57938325de32d8b97284c
amilne-mdsol <amilne@mdsol.com>
2023-06-16 16:15:24 +0100

    Setting up Challenge 11 - find the equation

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -9,13 +9,51 @@ import java.util.concurrent.TimeUnit
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 class Main {
 
+  /*
+  Challenge 11 - Find the equation
+
+  Given a sequence of integers and a sequence of operations, can you reach a specific target number by using the operations on the numbers?
+
+  The output from the function needs to be a string of an equation that gives the number.
+
+  Brackets needs to be around each operation, and there has to be a space between each item in the string.
+
+  You will need a bracket around the entire string output equation. This is all detailed in the example below and in the tests.
+
+  This is like the Countdown numbers game, but also including a list of operations you can use in the mix.
+
+  Note:
+  The is no single correct answer for each run. As long as your solution works mathematically to reach the number
+  and only uses items from the given input lists, it is an acceptable answer.
+  It does not need to be the best or simplest solution, just one that works.
+  Floating point numbers can be used in the working out of your solution, but your output should be an exact match of the target.
+  Each item in the input lists can only be used once, but there can be repeated items in the input lists.
+
+
+  Example:
+  input numbers: 3, 3, 9, 10, 50, 75
+  input operations: +, +, -, *, /
+  target = 783
+
+  This number can be done with some of the numbers and operations from the input.
+
+  75 + 3 = 78
+  78 * 10 = 780
+  780 + 3 = 783
+
+  To put this into the required format:
+  "( ( ( 75 + 3 ) * 10 ) + 3 )"
+   */
+
   @Benchmark
   def testBenchmarkFunction(bh: Blackhole): Unit = {
-    val w = ""
-    bh.consume(challengeFunction(w))
+    val i = Seq(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    val ops = Seq("*", "/", "+", "-")
+    val t = 10
+    bh.consume(challengeFunction(i, ops, t))
   }
 
-  def challengeFunction(i: String): Boolean = {
-    true
+  def challengeFunction(i: Seq[Int], o: Seq[String], t: Int): Option[String] = {
+    None
   }
 }
--- src/test/scala/example/ExampleSpec.scala
+++ src/test/scala/example/ExampleSpec.scala
@@ -6,7 +6,131 @@ class ExampleSpec extends FunSuite {
 
   val mainFunction = new Main()
 
+  private def parseEquation(input: String): Double = {
+    input
+      .split(" ")
+      .foldLeft((Seq[String](), Seq[Double]())) {
+        case ((ops: Seq[String], vals: Seq[Double]), value: String) =>
+          value match {
+            case "(" => (ops, vals)
+            case "+" => (value +: ops, vals)
+            case "-" => (value +: ops, vals)
+            case "*" => (value +: ops, vals)
+            case "/" => (value +: ops, vals)
+            case ")" =>
+              (ops.tail, (ops.head match {
+                case "+" => vals(1) + vals.head
+                case "-" => vals(1) - vals.head
+                case "*" => vals(1) * vals.head
+                case "/" => vals(1) / vals.head
+              }) +: vals.drop(2))
+            case item => (ops, item.toDouble +: vals)
+          }
+      }
+      ._2
+      .head
+  }
+
+  private def checkNumberUsage(setup: Seq[Int], output: String): Boolean = {
+    val parsedOutput = output.split(" ").toSeq.filterNot("()+-*/".contains(_)).map {
+      _.toDouble.toInt
+    }
+
+    val outputCounts = parsedOutput.distinct.map(x => (x, parsedOutput.count(_ == x)))
+
+    outputCounts.forall {
+      case (num, count) =>
+        val allowedNumber = setup.count(_ == num)
+        count <= allowedNumber
+    }
+  }
+
+  private def checkOperationUsage(setup: Seq[String], output: String): Boolean = {
+    val parsedSetup = output.split(" ").toSeq.filter("+-*/".contains(_))
+
+    val outputCounts = parsedSetup.distinct.map(x => (x, parsedSetup.count(_ == x)))
+
+    outputCounts.forall {
+      case (num, count) =>
+        val allowedNumber = setup.count(_ == num)
+        count <= allowedNumber
+    }
+  }
+
+  private def testFunction(input: Seq[Int], operations: Seq[String], target: Int, possible: Boolean) = {
+    val output = mainFunction.challengeFunction(input, operations, target)
+    if (possible) {
+      val parsedOutput = parseEquation(output.get)
+      assertEquals(parsedOutput, target.toDouble)
+      val checkedNumbers = checkNumberUsage(input, output.get)
+      assertEquals(checkedNumbers, true)
+      val checkOperations = checkOperationUsage(operations, output.get)
+      assertEquals(checkOperations, true)
+    } else {
+      assertEquals(output, None)
+    }
+  }
+
   test("challenge test 1") {
-    assertEquals(mainFunction.challengeFunction(""), true)
+    val input = Seq(3, 3, 9, 10, 50, 75)
+    val ops = Seq("+", "+", "-", "*", "/")
+    val target = 783
+
+    testFunction(input, ops, target, true)
+  }
+
+  test("challenge test 2") {
+    val input = Seq(3, 3, 9, 10, 50, 75)
+    val ops = Seq("+", "-", "*", "/")
+    val target = 783
+
+    testFunction(input, ops, target, false)
+  }
+
+  test("challenge test 3") {
+    val input = Seq(3, 3, 9, 10, 50, 75)
+    val ops = Seq("+", "-", "*", "/", "/")
+    val target = 783
+
+    testFunction(input, ops, target, true)
+  }
+
+  test("challenge test 4") {
+    val input = Seq(3, 3, 9, 10, 50, 75)
+    val ops = Seq("+", "-", "*", "/")
+    val target = 1970
+
+    testFunction(input, ops, target, false)
+  }
+
+  test("challenge test 5") {
+    val input = Seq(10, 10, 10, 10)
+    val ops = Seq("+", "+", "*", "*")
+    val target = 400
+
+    testFunction(input, ops, target, true)
+  }
+
+  test("challenge test 6") {
+    val input = Seq(10, 10, 10, 10)
+    val ops = Seq("+", "-", "*", "/")
+    val target = 400
+
+    testFunction(input, ops, target, false)
+  }
+
+  test("challenge test 7") {
+    val input = Seq(10, 40, 50)
+    val ops = Seq("-", "-")
+    val target = 20
+
+    testFunction(input, ops, target, true)
+  }
+
+  test("challenge test Performance 1") {
+    val input = Seq(6, 7, 8, 8, 11, 13, 16, 17, 19, 27, 80)
+
+    val ops = Seq("+", "+", "-", "/", "/", "*", "*", "*")
+    testFunction(input, ops, 1350, true)
   }
 }
